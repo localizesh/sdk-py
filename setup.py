760fd29a6d8aa479d558e3a6170c54573d6debd0
Vadim Homchik <homchik@gmail.com>
2026-03-31 17:28:24 +0200

    License (#7)

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     description="Localize.sh SDK",
     package_dir={"localizesh_sdk": "src"},
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='Apache-2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+    ],
     install_requires=[
         'protobuf>=5.26.1',
     ],
